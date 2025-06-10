d9ed6cd0467e793faaa8d0d1ccb78b59089e21c0
santippp <spriccofrakich@gmail.com>
2025-06-10 16:18:47 -0300

    Ej JugadorCansado Realizado

---
+++ JugadorCansado/JugadorCansado/Class2.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+public interface Jugador  
+{
+    bool Correr(int minutos);  
+    bool Cansado();
+    void Descansar(int minutos);
+
+    int Minutos { get; set; }  
+}
+
+public class Amateur : Jugador
+{
+    public int Minutos { get; set; }  
+
+    public Amateur()
+    {
+        Minutos = 20;  
+    }
+
+    public bool Correr(int minutos)
+    {
+        Minutos -= minutos;
+
+        if (Minutos < 0)
+        {
+            Minutos = 0;
+            return false;
+        }
+
+        return true;
+    }
+
+    public bool Cansado()
+    {
+        return (Minutos == 0);
+    }
+
+    public void Descansar(int minutos) 
+    {
+        Minutos += minutos;
+        if (Minutos > 20) Minutos = 20;
+    }
+}
+
+public class Profesional : Jugador
+{
+    public int Minutos { get; set; }
+
+    public Profesional()
+    {
+        Minutos = 40;  
+    }
+
+    public bool Correr(int minutos)
+    {
+        Minutos -= minutos;
+
+        if (Minutos < 0)
+        {
+            Minutos = 0;
+            return false;
+        }
+
+        return true;
+    }
+
+    public bool Cansado()
+    {
+        return (Minutos == 0);
+    }
+
+    public void Descansar(int minutos) 
+    {
+        Minutos += minutos;
+        if (Minutos > 40) Minutos = 40;
+    }
+}
---
+++ JugadorCansado/JugadorCansado/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+     
+        Jugador amateur = new Amateur();
+        Jugador profesional = new Profesional();
+
+        Console.WriteLine("Amateur: \n");
+        ProbarJugador(amateur, 25); 
+
+        // Probar Profesional
+        Console.WriteLine("Profesional:\n");
+        ProbarJugador(profesional, 50); 
+    }
+
+    static void ProbarJugador(Jugador jugador, int minutos)
+    {
+        Console.WriteLine($"Minutos de energía {jugador.Minutos} ");
+
+        Console.WriteLine($"\nIntento correr {minutos} minutos...");
+        bool exito = jugador.Correr(minutos);
+
+        if (exito)
+        {
+            Console.WriteLine("Corrio");
+        }
+
+        else
+            Console.WriteLine("No pudo correr");
+
+        Console.WriteLine($"minutos restantes: {jugador.Minutos}");
+        Console.WriteLine($"Cansado: {jugador.Cansado()}");
+
+       
+        int descanso = 15;
+        jugador.Descansar(descanso);
+        Console.WriteLine($"Recuperó {jugador.Minutos} minutos");
+
+    }
+}
