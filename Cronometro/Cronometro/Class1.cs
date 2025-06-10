abeb90668892210e77e38386fc87f49704f0ce58
santippp <spriccofrakich@gmail.com>
2025-06-10 15:20:55 -0300

    Ej Cronometro Realizado

---
+++ Cronometro/Cronometro/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public class Cronometro
+{
+	public int Minutos { get; set; } 
+    public int Segundos { get; set; }
+
+
+	public Cronometro()
+	{
+		Minutos = 0;
+		Segundos = 0;
+	}
+
+	public void Reiniciar()
+	{
+		Minutos = 0;
+		Segundos = 0;
+	}
+
+	public void IncrementarTiempo()
+	{
+		if (Segundos + 1 == 60)
+		{
+			Minutos++;
+			Segundos = 0;
+			return;
+		}
+
+		Segundos++;
+		return;
+	 }
+
+	public void MostrarTiempo()
+    {
+		Console.WriteLine("{0} : {1}", Minutos, Segundos);
+    }
+}
+
+
---
+++ Cronometro/Cronometro/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading; // Necesario para Thread.Sleep
+
+
+class Program
+{
+    static void Main()
+    {
+        Cronometro miCronometro = new Cronometro();
+
+        for (int i = 0; i <5000; i++)
+        {
+            miCronometro.MostrarTiempo();
+            miCronometro.IncrementarTiempo();
+            Thread.Sleep(1000);
+
+        }
+        
+    }
+}
