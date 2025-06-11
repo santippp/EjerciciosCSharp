148b99176872354bca7ef08017c097c813c299cc
Ignacio <130396251+ignacio-giova@users.noreply.github.com>
2025-06-11 14:07:56 -0300

    Ej Vehiculos Realizado

---
+++ Vehiculos/Vehiculos/Class1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+public interface IVehiculo
+{
+    // Metodos
+    void Mover(int minutos);
+    int DarPosicion();
+    void ReiniciarPosicion();
+
+    // Atributos
+    int Posicion { get; set; }
+    int Velocidad { get; set; }
+}
+
+public class Bicicleta : IVehiculo
+{
+    public int Posicion { get; set; }
+    public int Velocidad { get; set; }
+
+    public Bicicleta()
+    {
+        Posicion = 0;
+        Velocidad = 10;
+    }
+
+    public void Mover(int minutos)
+    {
+        Posicion += minutos * Velocidad;
+    }
+
+    public int DarPosicion()
+    {
+        return Posicion;
+    }
+
+    public void ReiniciarPosicion()
+    {
+        Posicion = 0;
+    }
+}
+
+public class Camion : IVehiculo
+{
+    public int Posicion { get; set; }
+    public int Velocidad { get; set; }
+
+    public Camion()
+    {
+        Posicion = 0;
+        Velocidad = 30;
+    }
+
+    public void Mover(int minutos)
+    {
+        Posicion += minutos * Velocidad;
+    }
+
+    public int DarPosicion()
+    {
+        return Posicion;
+    }
+
+    public void ReiniciarPosicion()
+    {
+        Posicion = 0;
+    }
+}
+
+public class Auto : IVehiculo
+{
+    public int Posicion { get; set; }
+    public int Velocidad { get; set; }
+
+    public Auto()
+    {
+        Posicion = 0;
+        Velocidad = 40;
+    }
+
+    //Constructor con Veloc. Distinta
+    public Auto(int velocidad)
+    {
+        Posicion = 0;
+        Velocidad = velocidad;
+    }
+
+
+    public void Mover(int minutos)
+    {
+        Posicion += minutos * Velocidad;
+    }
+
+    public int DarPosicion()
+    {
+        return Posicion;
+    }
+
+    public void ReiniciarPosicion()
+    {
+        Posicion = 0;
+    }
+}
---
+++ Vehiculos/Vehiculos/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Vehiculos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Definimos Objetos
+            Auto hondaFit = new Auto(45);
+            Bicicleta bici = new Bicicleta();
+            Camion camion = new Camion();
+
+            bici.Mover(20); // Mover la bicicleta durante 20 segundos
+            Console.WriteLine($"La bicicleta se movio {bici.DarPosicion()} metros");
+
+            bici.Mover(10); // Mover la bicicleta durante otros 10 segundos más
+            Console.WriteLine($"La bicicleta se movio {bici.DarPosicion()} metros");
+
+            hondaFit.Mover(30);
+            Console.WriteLine($"El Honda Fit se movio {hondaFit.DarPosicion()} metros");
+        }
+    }
+}
